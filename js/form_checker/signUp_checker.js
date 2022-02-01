2eef5525bf99d598cff916e70c3dba7800f2ca36
u1qns <u1qns@naver.com>
2022-02-01 20:04:08 +0900

    html 수정

--- js/form_checker/signUp_checker.js
+++ js/form_checker/signUp_checker.js
@@ -1,11 +1,29 @@
 var current_path= "/js/form_checker/sign_checker.js" ;
 console.info("Called %s", current_path);
+
 function join()
 {
   //입력한 정보의 유효성 검사
-  var login_form = document.login_info_form;
-  var id_ = login_form.sign_form_id_input.value;
-  var pw_ = login_form.sign_form_pw_input.value;
+  alert("Join")
+  var form = document.sign_up_form;
+  var email = form.input_email.value;
+  var pw1 = form.input_pw.value;
+  var pw2 = form.input_pw_match.value;
+  if(pw1 !== pw2)
+  {
+    console.log("입력하신 비밀번호가 서로 맞지 않습니다.")
+  }
+  var name = form.input_name.value;
+  var birthday = form.input_birthday.value;
+  var nation_num = form.nation_num.value;
+  var phone_num = form.input_phone_num.value;
 
+  console.log(email)
+  console.log(pw)
+  console.log(name)
+  console.log(birthday)
+  console.log(nation_num)
+  console.log(phone_num)
 
+  //location.href='home.html';
 }
